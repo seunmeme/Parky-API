fb56c3b65d34cb14147e156f3ad9f7b7b1f5e802
Seun Odewale <seunodewale.zone5@gmail.com>
2021-02-16 15:19:21 +0100

    add repository for NationalPark

---
+++ ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> GetNationalParks();
+
+        NationalPark GetNationalPark(int nationalParkId);
+
+        bool NationalParkExists(string name);
+
+        bool NationalParkExists(int id);
+
+        bool CreateNationalPark(NationalPark nationalPark);
+
+        bool UpdateNationalPark(NationalPark nationalPark);
+
+        bool DeleteNationalPark(NationalPark nationalPark);
+
+        bool Save();
+
+    }
+}
