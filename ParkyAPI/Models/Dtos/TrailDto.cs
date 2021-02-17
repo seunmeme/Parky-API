7b5db89a0f3b2f7099808d5b2814ee277ab809e4
Seun Odewale <seunodewale.zone5@gmail.com>
2021-02-17 23:57:42 +0100

    add Trail model, Dto and migrations

---
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using static ParkyAPI.Models.Trail;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailDto
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public DifficultyType Difficulty { get; set; }
+
+        public int NationalParkId { get; set; }
+
+        public NationalParkDto NationalPark { get; set; }
+    }
+}
